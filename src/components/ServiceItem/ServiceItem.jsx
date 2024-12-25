355da4566aa30aaec8d9c61be6cf1ab441f29663
adilFromAstana <adilfirstus@gmail.com>
2024-12-26 00:02:17 +0600

    d

--- src/components/ServiceItem/ServiceItem.jsx
+++ src/components/ServiceItem/ServiceItem.jsx
@@ -19,25 +19,27 @@ const ServiceItem = ({ title, description, index, imageUrl }) => {
   }, []);
 
   useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        const visiblePercentage = Math.min(
-          Math.max(entry.intersectionRatio, 0),
-          1
-        );
-        setVisibility(visiblePercentage); // Устанавливаем процент видимости
-      },
-      {
-        threshold: Array.from({ length: 101 }, (_, i) => i / 100), // Генерируем массив от 0 до 1 с шагом 0.01
-      }
-    );
+    if (!isMobile) {
+      const observer = new IntersectionObserver(
+        ([entry]) => {
+          const visiblePercentage = Math.min(
+            Math.max(entry.intersectionRatio, 0),
+            1
+          );
+          setVisibility(visiblePercentage); // Устанавливаем процент видимости
+        },
+        {
+          threshold: Array.from({ length: 101 }, (_, i) => i / 100), // Генерируем массив от 0 до 1 с шагом 0.01
+        }
+      );
 
-    const element = document.querySelector(`#service-${index}`);
-    if (element) observer.observe(element);
+      const element = document.querySelector(`#service-${index}`);
+      if (element) observer.observe(element);
 
-    return () => {
-      if (element) observer.unobserve(element);
-    };
+      return () => {
+        if (element) observer.unobserve(element);
+      };
+    }
   }, [index]);
 
   const opacityValue = Math.min(visibility * 2, 1); // Плавное увеличение прозрачности
@@ -45,16 +47,16 @@ const ServiceItem = ({ title, description, index, imageUrl }) => {
     visibility < 0.5
       ? `${(1 - visibility) * 80}%` // Двигается меньше на первых 50%
       : isMobile
-      ? `0%` // Для телефонов
-      : `45%`; // Для больших экранов
+        ? `0%` // Для телефонов
+        : `45%`; // Для больших экранов
 
   return (
     <div
       id={`service-${index}`}
       className="service-item"
       style={{
-        transform: `translateX(${index % 2 === 0 ? "-" : ""}${translateValue})`,
-        opacity: opacityValue,
+        transform: isMobile ? 'inherit' : `translateX(${index % 2 === 0 ? "-" : ""}${translateValue})`,
+        opacity: isMobile ? '1' : opacityValue,
       }}
     >
       <div
